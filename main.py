63c05f23ad207e395ead6a5691b753dbcf9d3fe4
Emmanuel Herrera <emherrer1@gmail.com>
2023-06-30 01:20:00 -0400

    initial commit: pdf generator

---
+++ main.py
@@ -0,0 +1,42 @@
+from fpdf import FPDF
+import pandas as pd
+
+# Instancia PDF
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+# Archivo CSV
+df = pd.read_csv("topics.csv")
+
+for idx, row in df.iterrows():
+
+    # Set the header
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+
+    # Add multiple lines
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
+
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for page in range(row["Pages"]-1):
+        pdf.add_page()
+
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # Add multiple lines
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
+
+pdf.output("output.pdf")
